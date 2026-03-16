352274e10c6e66064df81c817edb52e56b80b3a1
maimuna-t-333 <maimuna.tabr@gmail.com>
2026-03-16 15:12:45 +0600

    text motion

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-gsap.to("#box1",{
-    x:1000,
+// gsap.to("#box1",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"gray",
+//     borderRadius:"50%",
+//     scale:1.25
+// })
+// gsap.from("#box2",{
+//     x:500,
+//     y:500,
+//     duration:2,
+//     delay:1,
+//     rotate:360
+// })
+
+gsap.from("h1",{
+    // color:"red",
+    opacity:0,
+    y:30,
     duration:2,
     delay:1,
-    rotate:360,
-    backgroundColor:"gray",
-    borderRadius:"50%",
-    scale:1.25
-})
-gsap.from("#box2",{
-    x:500,
-    y:500,
-    duration:2,
-    delay:1,
-    rotate:360
+    stagger:0.5
 })
