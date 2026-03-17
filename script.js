f0cd147f732453c6cc89a47e279d7ce262221147
maimuna-t-333 <maimuna.tabr@gmail.com>
2026-03-17 20:27:56 +0600

    ScrollTrigger

--- script.js
+++ script.js
@@ -101,15 +101,62 @@
 //     scale:0.5
 // })
 
-gsap.from("#page1 #box",{
-    scale:0,
-    delay:1,
-    duration:2,
-    rotate:360
-})
+// gsap.from("#page1 #box",{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:360
+// })
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     duration:2,
+//     rotate:360,
+//     // srollTrigger:"#page2 #box"
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%"
+//     }
+// })
+
+// gsap.from("#page2 h1",{
+//     opacity:0,
+//     duration:2,
+//     x:500,
+//     scrollTrigger:{
+//         trigger:"#page2 h1",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 50%"
+//     }
+// })
+// gsap.from("#page2 h2",{
+//     opacity:0,
+//     duration:2,
+//     x:-500,
+//     scrollTrigger:{
+//         trigger:"#page2 h2",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 50%"
+//     }
+// })
+
+
+
 gsap.from("#page2 #box",{
     scale:0,
-    delay:2,
-    duration:2,
-    rotate:360
+    opacity:0,
+    duration:1,
+    rotate:720,
+    scrollTrigger:{
+        trigger:"#page2 #box",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+        end:"top 10%",
+        scrub:2,
+        
+    }
 })
