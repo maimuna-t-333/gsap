307124237d80050c1d4d7e7c3d53ed343fdf7a8b
maimuna-t-333 <maimuna.tabr@gmail.com>
2026-03-17 15:02:21 +0600

    Added scroll trigger cdn

--- script.js
+++ script.js
@@ -79,24 +79,37 @@
 //    duration:1.5,
 // })
 
-var tl=gsap.timeline();
+// var tl=gsap.timeline();
 
-tl.from("h2",{
-    y:-20,
-    opacity:0,
-    duration:1,
-    delay:0.5
-},)
-tl.from("h4",{
-    y:-20,
-    opacity:0,
-    duration:1,
-    stagger:0.3
-},)
+// tl.from("h2",{
+//     y:-20,
+//     opacity:0,
+//     duration:1,
+//     delay:0.5
+// },)
+// tl.from("h4",{
+//     y:-20,
+//     opacity:0,
+//     duration:1,
+//     stagger:0.3
+// },)
 
-tl.from("h1",{
-    y:20,
-    opacity:0,
-    duration:1,
-    scale:0.5
+// tl.from("h1",{
+//     y:20,
+//     opacity:0,
+//     duration:1,
+//     scale:0.5
+// })
+
+gsap.from("#page1 #box",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:360
+})
+gsap.from("#page2 #box",{
+    scale:0,
+    delay:2,
+    duration:2,
+    rotate:360
 })
